7b9fc05f74c5fe61f785e050d902f838d884e23b
Yeom <ydh7219@naver.com>
2023-05-15 20:50:31 +0900

    model register

--- BlogProject/post/admin.py
+++ BlogProject/post/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment, FreePost, FreeComment, Tag, Category
+from .models import Post, Comment, FreePost, FreeComment, Tag, Category, Blog
 
 # Register your models here.
 
@@ -7,5 +7,6 @@ admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Tag)
 admin.site.register(Category)
+admin.site.register(Blog)
 # admin.site.register(FreePost)
 # admin.site.register(FreeComment)
